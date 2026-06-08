d6c7f73e56b1db511df752500a4e7c20b759d5e2
alexwolf00 <119752152+alexwolf00@users.noreply.github.com>
2026-06-08 01:14:03 +0100

    add sprite groups, include player

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     try:
@@ -28,10 +34,11 @@ def main():
                 if event.type == pygame.QUIT:
                     return
 
-            player.update(dt)
+            updatable.update(dt)
 
             screen.fill("black")
-            player.draw(screen)
+            for obj in drawable:
+                obj.draw(screen)
             pygame.display.flip()
 
             # limit the framerate to 60 FPS
