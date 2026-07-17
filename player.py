2c158a48b39e2b9b29ddb03a9c73f59dc28e71d2
alexwolf00 <119752152+alexwolf00@users.noreply.github.com>
2026-07-17 15:39:04 +0100

    add player shooting

--- player.py
+++ player.py
@@ -1,6 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import (
+    LINE_WIDTH,
+    PLAYER_RADIUS,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+    PLAYER_SHOOT_SPEED,
+)
 
 
 class Player(CircleShape):
@@ -19,8 +26,18 @@ class Player(CircleShape):
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
     def rotate(self, dt: float) -> None:
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def shoot(self) -> None:
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
@@ -34,8 +51,5 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
 
-    def move(self, dt: float) -> None:
-        unit_vector = pygame.Vector2(0, 1)
-        rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-        self.position += rotated_with_speed_vector
+        if keys[pygame.K_SPACE]:
+            self.shoot()
